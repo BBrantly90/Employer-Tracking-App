dc584c3054029c06c70a43e420cf034dff43a65f
BBran <Bryan4690@gmail.comgit config --global user.name BBrangit config --global user.email Bryan4690@gmail.com>
2024-04-22 19:46:50 -0400

    added the code for collecting employee first and last name, along with employee role

--- index.js
+++ index.js
@@ -81,6 +81,96 @@ var employee_tracker = function () {
                         }
                     },
                     {
+                        // Adding Salary
+                        type: 'input',
+                        name: 'salary',
+                        message: 'What is the salary of the role?',
+                        validate: salaryInput => {
+                            if (salaryInput) {
+                                return true;
+                            } else {
+                                console.log('Please Add A Salary!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        // Department
+                        type: 'list',
+                        name: 'department',
+                        message: 'Which department does the role belong to?',
+                        choices: () => {
+                            var array = [];
+                            for (var i = 0; i < result.length; i++) {
+                                array.push(result[i].name);
+                            }
+                            return array;
+                        }
+                    }
+                ]).then((answers) => {
+                    // Compares result and stores it into the variable
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].name === answers.department) {
+                            var department = result[i];
+                        }
+                    }
+
+                    db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [answers.role, answers.salary, department.id], (err, result) => {
+                       if (err) throw err;
+                       console.log('Added ${answers.role} to the database.')
+                       employee_tracker(); 
+                    });
+                })
+            });
+        } else if (answers.prompt === 'Add AN Employee') {
+            // Calling database to acquire roles and managers
+            db.query('SELECT * FROM employee, role', (err, result) => {
+                if (err) throw err;
+
+                inquirer.prompt([
+                    {
+                        // Adds Employee First Name
+                        type: 'input',
+                        name: 'firstName',
+                        message: 'What is the employees first name?',
+                        validate: firstNameInput => {
+                            if (firstNameInput) {
+                                return true;
+                            } else {
+                                console.log('Please Add A First Name!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        // Adds Employee Last Name
+                        type: 'input',
+                        name: 'lastName',
+                        message: 'What is the employees last name?',
+                        validate: lastNameInput => {
+                            if (lastNameInput) {
+                                return true;
+                            } else {
+                                console.log('Please Add A Salary!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        // Adds Employee Role
+                        type: 'list',
+                        name: 'role',
+                        message: 'What is the employees role?',
+                        choices: () => {
+                            var array = [];
+                            for (var i = 0; i < result.length; i++) {
+                                array.push(result[i].title);
+                            }
+                            var newArray = [...new Set(array)];
+                            return newArray;
+                        }
+                    },
+                    {
                         
                     }
                 ])
