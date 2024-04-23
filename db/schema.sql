f80a485840542ae5c4232cd39805c902e7c9f468
BBran <Bryan4690@gmail.comgit config --global user.name BBrangit config --global user.email Bryan4690@gmail.com>
2024-04-23 10:04:01 -0400

    Corrected syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,23 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(30)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INTEGER
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
 );
 
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
 );
